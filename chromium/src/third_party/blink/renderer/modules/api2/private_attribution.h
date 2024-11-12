a40e3108ea93239ae30d53fdb4510c68b15a6972
goutzou <ngoutzoulias@gmail.com>
2024-11-12 11:28:04 -0500

    finished skeleton

--- chromium/src/third_party/blink/renderer/modules/api2/private_attribution.h
+++ chromium/src/third_party/blink/renderer/modules/api2/private_attribution.h
@@ -2,22 +2,41 @@
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_API2_PRIVATE_ATTRIBUTION_H_
 
 #include "third_party/blink/renderer/modules/api2/private_attribution_types.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
-class BudgetRequestOptions;
-class BudgetDelegatorResponse;
-class PrivateAttributionConversionOptions;
+class ExecutionContext;
+class AdTechBudgetConfig;
+class FirstPartyBudgetAllocationOptions;
+class API2ReportRequest;
+class API2ReportResponse;
 
-class PrivateAttribution {
+class PrivateAttribution final : public ScriptWrappable {
+  DEFINE_WRAPPERTYPEINFO();
+
  public:
-  PrivateAttribution();
-  virtual ~PrivateAttribution();
+  explicit PrivateAttribution(ExecutionContext*);
+  ~PrivateAttribution() override;
 
-  virtual void GetDelegatedBudget(const BudgetRequestOptions& options);
-  virtual void MeasureConversion(const PrivateAttributionConversionOptions& options);
+  virtual void ConfigureAdTechBudgets(
+      const FirstPartyBudgetAllocationOptions& options);
+
+  // Ad-tech requests report
+  virtual void RequestAPI2Report(const API2ReportRequest& options);
+
+  // garbage collection
+  void Trace(Visitor*) const override;
+
+ private:
+  // Store the execution context for security checks
+  Member<ExecutionContext> execution_context_;
+  
+  // Budget management state
+  // TODO: Add data structures to track budget allocations
 };
 
 }  
-
-#endif 
---
+++ chromium/src/third_party/blink/renderer/modules/api2/private_attribution_types.cc
@@ -0,0 +1,19 @@
+#include "third_party/blink/renderer/modules/api2/private_attribution_types.h"
+
+namespace blink {
+
+AdTechBudgetConfig::AdTechBudgetConfig()
+    : allocated_budget_(0.0) {}
+
+FirstPartyBudgetAllocationOptions::FirstPartyBudgetAllocationOptions() {}
+
+API2ReportRequest::API2ReportRequest()
+    : minimum_budget_(0.0)
+    , desired_budget_(0.0)
+    , histogram_size_(0) {}
+
+
+API2ReportResponse::API2ReportResponse()
+    : is_sufficient_(false) {}
+
+}  
